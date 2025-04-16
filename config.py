e022194d8e2be5cb1852fde3f534609e1409c78c
Deseoai <info@deseoai.com>
2025-04-16 14:15:33 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
-# Hole die Datenbankverbindung aus der Umgebungsvariable
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if not DATABASE_URL:
+if DATABASE_URL is None:
     raise ValueError("DATABASE_URL is not set. Please define it as an environment variable.")
